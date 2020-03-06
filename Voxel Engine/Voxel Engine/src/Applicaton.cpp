3be6dfd05dc34852492aed3e31b06927abf77acd
Arjen Simons <arjen.simons@gmail.com>
2020-03-06 18:27:31 +0100

    Made sure the triangle is rendered using modern OpenGL

--- Voxel Engine/Voxel Engine/src/Applicaton.cpp
+++ Voxel Engine/Voxel Engine/src/Applicaton.cpp
@@ -1,20 +1,31 @@
+#include <GL/glew.h>
 #include "Window.h"
 #include <iostream>
 
 int main(void)
 {
 	Window window = Window::Window(WindowProps());
+
+	float positions[6]{
+		-0.5f, -0.5,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
+
+	unsigned int buffer;
+	glGenBuffers(2, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
 	while (window.IsRunning())
 	{
-		/* Render here */
+		/*Render*/
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
-
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		window.OnUpdate();
 	}
 
