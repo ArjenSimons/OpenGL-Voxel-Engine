85de64e64d388f7e0d301d911f4d1913fa4353d0
Arjen Simons <arjen.simons@gmail.com>
2020-03-06 18:27:04 +0100

    Improved some error handeling

--- Voxel Engine/Voxel Engine/Window.cpp
+++ Voxel Engine/Voxel Engine/Window.cpp
@@ -5,25 +5,32 @@
 Window::Window(const WindowProps& props)
 {
 	if (!glfwInit())
+	{
 		std::cout << "Could not initialize GLFW!" << std::endl;
+		exit(EXIT_FAILURE);
+	}
 
 	data.Title = props.Title;
 	data.Width = props.Width;
 	data.Height = props.Height;
 
 	window = glfwCreateWindow(props.Width, props.Height, props.Title, NULL, NULL);
+	printf("Creating %s window | Width: %i  height: %i\n", props.Title, props.Width, props.Height);
 	if (!window)
 	{
 		std::cout << "Error initializeng window and OpenGL context" << std::endl;
 		Shutdown();
 	}
 
-	//Make window the current context
 	glfwMakeContextCurrent(window);
 
 	GLenum err = glewInit();
 	if (err != GLEW_OK)
+	{
 		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+		Shutdown();
+		exit(EXIT_FAILURE);
+	}
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 }
