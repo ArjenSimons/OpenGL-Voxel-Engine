eb8127059f16f1e201e76c7729d52bbf6fccd913
Arjen Simons <arjen.simons@gmail.com>
2020-03-07 18:27:53 +0100

    Rendered a rectangle

--- Voxel Engine/Voxel Engine/src/Applicaton.cpp
+++ Voxel Engine/Voxel Engine/src/Applicaton.cpp
@@ -85,16 +85,20 @@ int main(void)
 {
 	Window window = Window::Window(WindowProps());
 
-	float positions[6]{
+	float positions[12]{
 		-0.5f, -0.5,
-		 0.0f,  0.5f,
-		 0.5f, -0.5f
+		 0.5f, -0.5f,
+		-0.5f,  0.5f,
+
+		 0.5f, -0.5f,
+		 0.5f,  0.5f,
+		-0.5f,  0.5f
 	};
 
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
@@ -110,8 +114,8 @@ int main(void)
 		/*Render*/
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		window.OnUpdate();
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		window.OnUpdate();	
 	}
 
 	glDeleteProgram(shader);
